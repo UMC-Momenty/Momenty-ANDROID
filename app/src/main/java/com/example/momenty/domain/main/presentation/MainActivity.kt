51eb36f1b818adf5622e3eee38f7e2525964f0f0
Administrator <kdy99ky@gmail.com>
2026-01-18 16:37:39 +0900

    [feat] 바텀 네비게이션 연결

--- app/src/main/java/com/example/momenty/domain/main/presentation/MainActivity.kt
+++ app/src/main/java/com/example/momenty/domain/main/presentation/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.momenty.domain.main.presentation
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.momenty.R
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
-
-
-
 }
