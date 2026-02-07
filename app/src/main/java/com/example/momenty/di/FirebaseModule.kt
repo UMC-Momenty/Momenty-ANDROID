be0bad86800a43bb8f0c33fbacf17eba76e489b2
Administrator <kdy99ky@gmail.com>
2026-02-08 00:41:35 +0900

    [feat] 커뮤니티 작성 UI 구현

--- app/src/main/java/com/example/momenty/di/FirebaseModule.kt
+++ app/src/main/java/com/example/momenty/di/FirebaseModule.kt
@@ -1,10 +1,7 @@
 package com.example.momenty.di
 
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.firestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,13 +14,11 @@ object FirebaseModule {
 
     @Provides
     @Singleton
-    fun provideFirebaseAuth(): FirebaseAuth {
-        return Firebase.auth
-    }
+    fun provideFirebaseAuth(): FirebaseAuth =
+        FirebaseAuth.getInstance()
 
     @Provides
     @Singleton
-    fun provideFirebaseFirestore(): FirebaseFirestore {
-        return Firebase.firestore
-    }
+    fun provideFirebaseFirestore(): FirebaseFirestore =
+        FirebaseFirestore.getInstance()
 }
