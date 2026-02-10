9da2fe76cb89a844019e8c36a962ccc8fe6ffec1
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-02-10 22:24:30 +0900

    UI: 알림 추가 페이지, 활동 유형 dialog

---
+++ app/src/main/java/com/example/momenty/domain/calendar/AddAlarmFragment.kt
@@ -0,0 +1,10 @@
+package com.example.momenty.domain.calendar
+
+import androidx.fragment.app.Fragment
+import com.example.momenty.databinding.FragmentAddAlarmBinding
+
+class AddAlarmFragment : Fragment() {
+
+    private var _binding: FragmentAddAlarmBinding? = null
+    private val binding get() = _binding!!
+}
