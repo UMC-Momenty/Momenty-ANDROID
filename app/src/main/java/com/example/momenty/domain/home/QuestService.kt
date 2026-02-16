ad3742c2d37f638f4ad0868d07a6639884232d96
kang4094 <haengoo@mme.dongguk.edu>
2026-02-17 06:09:29 +0900

    Chore: 기존의 임의의 Response 데이터 클래스 대신 BaseResponse 사용 통일

--- app/src/main/java/com/example/momenty/domain/home/QuestService.kt
+++ app/src/main/java/com/example/momenty/domain/home/QuestService.kt
@@ -1,5 +1,6 @@
 package com.example.momenty.domain.home
 
+import com.example.momenty.global.api.BaseResponse
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
@@ -12,12 +13,12 @@ interface QuestService {
     //fun loadQuest(@Header("Authorization") token: String): Call<LoadQuestResponse>
     suspend fun loadQuest(
         @Header("Authorization") token: String
-    ): Response<LoadQuestResponse<LoadQuestData>>
+    ): Response<BaseResponse<LoadQuestData>>
 
     @POST("api/quest")
     suspend fun writeQuest(
         @Header("Authorization") token: String,
         @Body req: WriteQuestRequest
-    ): Response<WriteQuestResponse<WriteQuestData>>
+    ): Response<BaseResponse<WriteQuestData>>
     //fun writeQuest(@Body writeQuestRequest: WriteQuestRequest): Call<WriteQuestResponse>
 }
