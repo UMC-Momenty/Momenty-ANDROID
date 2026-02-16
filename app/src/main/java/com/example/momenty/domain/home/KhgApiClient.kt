4ce79bed0e55508587467822c9bff5be3966da0b
kang4094 <haengoo@mme.dongguk.edu>
2026-02-17 06:08:03 +0900

    Rename: QuestApiClient.kt -> KhgApiClient.kt

--- app/src/main/java/com/example/momenty/domain/home/KhgApiClient.kt
+++ app/src/main/java/com/example/momenty/domain/home/KhgApiClient.kt
@@ -1,12 +1,13 @@
 package com.example.momenty.domain.home
 
+import com.example.momenty.domain.mypage.MyPageService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-object QuestApiClient {
+object KhgApiClient {
     private val BASE_URL = "https://api.momenty.com/"
 
     private val logging = HttpLoggingInterceptor().apply {
@@ -27,4 +28,5 @@ object QuestApiClient {
         .build()
 
     val questService: QuestService = retrofit.create(QuestService::class.java)
+    val myPageService: MyPageService = retrofit.create(MyPageService::class.java)
 }
